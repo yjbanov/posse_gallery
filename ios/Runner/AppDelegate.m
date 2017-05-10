9565b2ffaedc7af30f140df59f9f4c863c007a95
Albert Lardizabal <albie06@gmail.com>
2017-05-10 18:14:34 -0400

    iOS PathProvider linker issue

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,8 +1,9 @@
 #include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+	[GeneratedPluginRegistrant registerWithRegistry:self];
     return YES;
 }
 
