415965c3173d523de83e0222ce833e7ee86d66e2
Luke <freeman.luke@gmail.com>
2017-05-10 13:13:31 -0700

    fixes and asset reduction

--- android/app/src/main/java/com/yourcompany/posse_gallery/MainActivity.java
+++ android/app/src/main/java/com/yourcompany/posse_gallery/MainActivity.java
@@ -2,10 +2,12 @@ package com.yourcompany.posse_gallery;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
     }
 }
